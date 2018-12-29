9d446e2e865a2bfd1034775fe0938d561a3ddee3
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-12-29 01:17:21 -0500

    Update TextGame.java

--- src/TextGame.java
+++ src/TextGame.java
@@ -1,9 +1,26 @@
+import java.util.Random;
 
 public class TextGame {
 	
 	//the grid boi
 	static int[][] gameGrid = new int[4][4];
 	
+	public static void startingTiles() {
+		boolean blank = false;
+		for(int i = 0; i<2; i++) {
+			while(!blank) {
+				// create instance of Random class and finds random tile
+				Random rand = new Random(); 
+				int randRow = rand.nextInt(16);	
+				int randCol = rand.nextInt(16);	
+			}
+		}
+	}
+	public static void runGame() {
+		
+	}
+	
+	//Prints game in grid format
 	public static void printGame() {
 		for (int i = 0; i < gameGrid.length; i++) {
 	        for(int j = 0; j < gameGrid[i].length; j++) {
