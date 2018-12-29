ed24e7ae5567a8c09803b9a1f7d9c7c9181656b6
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-12-29 00:46:20 -0500

    Update TextGame.java

--- src/TextGame.java
+++ src/TextGame.java
@@ -2,12 +2,21 @@
 public class TextGame {
 	
 	//the grid boi
-	int[][] gameGrid = new int[4][4];
+	static int[][] gameGrid = new int[4][4];
+	
+	public static void printGame() {
+		for (int i = 0; i < gameGrid.length; i++) {
+	        for(int j = 0; j < gameGrid[i].length; j++) {
+	           System.out.print(gameGrid[i][j] + " ");
+	        }
+	        System.out.println("");
+	     }
+	}
 
 	public static void main(String[] args) {
 		// Alright sluts, we're making this textbased first
+		printGame();
 		
-
 	}
 
 }
