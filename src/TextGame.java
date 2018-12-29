f84520b0c8a8f875ce402aae2bf696e9a9a4a03b
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-12-29 00:36:31 -0500

    Update TextGame.java

--- src/TextGame.java
+++ src/TextGame.java
@@ -1,8 +1,12 @@
 
 public class TextGame {
+	
+	//the grid boi
+	int[][] gameGrid = new int[4][4];
 
 	public static void main(String[] args) {
 		// Alright sluts, we're making this textbased first
+		
 
 	}
 
